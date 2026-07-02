86284489dae5b02d54f7c659e0c554339d0e5375
Muhammad Taha <tahabutt627@gmail.com>
2026-07-02 23:25:36 +0500

    Cars Data added

---
+++ car-rental-frontend/src/data/cars.ts
@@ -0,0 +1,28 @@
+export const cars = [
+  {
+    id: 1,
+    name: "BMW X5",
+    type: "SUV",
+    price: 90,
+    image:
+      "https://images.unsplash.com/photo-1555215695-3004980ad54e?w=800",
+  },
+
+  {
+    id: 2,
+    name: "Tesla Model 3",
+    type: "Electric",
+    price: 110,
+    image:
+      "https://images.unsplash.com/photo-1560958089-b8a1929cea89?w=800",
+  },
+
+  {
+    id: 3,
+    name: "Audi A6",
+    type: "Luxury Sedan",
+    price: 100,
+    image:
+      "https://images.unsplash.com/photo-1606664515524-ed2f786a0bd6?w=800",
+  },
+];
