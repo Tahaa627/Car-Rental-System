ee2eb911610ffe84125d11c76cac55a0b8a96b9f
Muhammad Taha <tahabutt627@gmail.com>
2026-07-02 23:49:57 +0500

    Implemented FeaturedCars lOGIC

---
+++ car-rental-frontend/src/components/FeaturedCars/FeaturedCars.tsx
@@ -0,0 +1,43 @@
+import "./FeaturedCars.css";
+
+import CarCard from "../CarCard";
+
+import { cars } from "../../data/cars";
+
+const FeaturedCars = () => {
+  return (
+    <section className="featured">
+
+      <h2>Featured Cars</h2>
+
+      <p>
+        Browse our premium collection
+      </p>
+
+      <div className="cars-grid">
+
+        {cars.map((car) => (
+
+          <CarCard
+
+            key={car.id}
+
+            name={car.name}
+
+            type={car.type}
+
+            price={car.price}
+
+            image={car.image}
+
+          />
+
+        ))}
+
+      </div>
+
+    </section>
+  );
+};
+
+export default FeaturedCars;
