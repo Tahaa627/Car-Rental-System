5f01c20ef3ce739e9703cb3f2c1bb0e89631d335
Muhammad Taha <tahabutt627@gmail.com>
2026-07-03 19:06:44 +0500

    Implemented Container

---
+++ car-rental-frontend/src/components/Container/Container.tsx
@@ -0,0 +1,22 @@
+import "./Container.css";
+import type { ReactNode } from "react";
+
+interface ContainerProps{
+    children:ReactNode;
+}
+
+const Container=({children}:ContainerProps)=>{
+
+    return(
+
+        <div className="container">
+
+            {children}
+
+        </div>
+
+    );
+
+}
+
+export default Container;
